f8f18cf1160b6522f4e501bbd829e153c5aef664
akslk00 <akakquswodud31@gmail.com>
2023-11-24 12:50:47 +0900

    Update app_eda.py

--- app_eda.py
+++ app_eda.py
@@ -5,17 +5,17 @@ import pandas as pd
 def run_eda_app():
     st.subheader('주택 가격데이터')
     df=pd.read_csv('./data/housing_price_dataset.csv')
-    df=df.sample(n=10000,random_state=1000)
     df=df.loc[df['Price']>0,]
+    #df=df.sample(n=10000,random_state=1000)
     st.dataframe(df)
 
     st.text('평수가 가장 넓은집의 스펙모음')
-    square=df.loc[df['SquareFeet']==2999,'Bedrooms':'Price']
+    square=df.loc[df['SquareFeet']==2999,]
     st.dataframe(square)
 
 
     st.text('평수가 가장 좁은집의 스펙모음')
-    squaremin=df.loc[df['SquareFeet']==1000,'Bedrooms':'Price']
+    squaremin=df.loc[df['SquareFeet'] == 1000,]
     st.dataframe(squaremin)
 
     
@@ -52,12 +52,9 @@ def run_eda_app():
     Pr=st.slider('가격', 154, 492196,value=154)
     BD=st.slider('침실', 2, 5, value=2)
     BS=st.slider('화장실', 1, 3, value=1)
-    sf=df.loc[df['SquareFeet']>=SF,]
-    pr=df.loc[df['Price']>=Pr,]
-    bd=df['Bedrooms']>=BD
-    bs=df['Bathrooms']>=BS
-    sfpr=df.loc[df[sf & pr],]
-    
-    st.dataframe(sfpr)
-    
-    
+    sf=df['SquareFeet']<=SF
+    pr=df['Price']<=Pr
+    bd=df['Bedrooms']<=BD
+    bs=df['Bathrooms']<=BS
+    ALl=df.loc[((bd&bs)&(sf&pr)),]
+    st.dataframe(ALl)
